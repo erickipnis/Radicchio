d5763a33e92069278bb1a7758eed77e00b1434b7
Eric Kipnis <esk8107@rit.edu>
2016-01-08 18:46:18 -0500

    Change functions to include sets for keeping track of timers

--- src/lua/start.lua
+++ src/lua/start.lua
@@ -1,1 +1,2 @@
-return redis.call('PSETEX', KEYS[1], ARGV[1], '');
+redis.call('HSET', KEYS[1], KEYS[2], '')
+return redis.call('PSETEX', KEYS[2], ARGV[1], '')
--- test/unit/radicchio.test.js
+++ test/unit/radicchio.test.js
@@ -4,6 +4,8 @@ import Promise from 'bluebird';
 import radicchio from '../../src/radicchio';
 
 const expect = chai.expect;
+const setSuffix = '-set';
+const fieldSuffix = '-field';
 
 describe('Radicchio_Tests', () => {
 
@@ -16,7 +18,9 @@ describe('Radicchio_Tests', () => {
 
   describe('#startTimer', () => {
     it('Should store the timer key in Redis', (done) => {
-      radicchio.startTimer('abc123', '10000')
+      const id = 'abc123';
+
+      radicchio.startTimer(id + setSuffix, id + fieldSuffix, '10000')
       .then((result) => {
         expect(result).to.equal(true);
         done();
@@ -26,7 +30,9 @@ describe('Radicchio_Tests', () => {
 
   describe('#disableTimer', () => {
     it('Should delete the timer key in Redis', (done) => {
-      radicchio.disableTimer('abc123')
+      const id = 'abc123';
+
+      radicchio.disableTimer(id + setSuffix, id + fieldSuffix)
       .then((result) => {
         expect(result).to.equal(1);
         done();
@@ -36,9 +42,11 @@ describe('Radicchio_Tests', () => {
 
   describe('#getTimeLeft', () => {
     it('Should get the time to live on a timer id', (done) => {
-      radicchio.startTimer('abc1234', '10000')
+      const id = 'abc1234';
+
+      radicchio.startTimer(id + setSuffix, id + fieldSuffix, '10000')
       .then(() => {
-        radicchio.getTimeLeft('abc1234')
+        radicchio.getTimeLeft(id + fieldSuffix)
         .then((result) => {
           expect(result).to.be.at.least(0);
           done();
@@ -49,12 +57,14 @@ describe('Radicchio_Tests', () => {
 
   describe('#getTimeLeftOnAllKeys', () => {
     it('Should get the time to live on an array of timer ids', (done) => {
+      const id1 = 'abc12345';
+      const id2 = 'abc123456';
       Promise.all([
-        radicchio.startTimer('abc12345', '10000'),
-        radicchio.startTimer('abc123456', '10000'),
+        radicchio.startTimer(id1 + setSuffix, id1 + fieldSuffix, '10000'),
+        radicchio.startTimer(id1 + setSuffix, id2 + fieldSuffix, '10000'),
       ])
       .then(() => {
-        radicchio.getTimeLeftOnAllKeys(['abc12345', 'abc123456'])
+        radicchio.getTimeLeftOnSetKeys(id1 + setSuffix)
         .then((results) => {
           expect(results).to.have.length(2);
           done();
