8ae62ec61fef62fa92787b17b72e370f4c0b246a
Eric Kipnis <esk8107@rit.edu>
2016-01-08 00:40:51 -0500

    Add getting ttl on single and all keys

--- src/radicchio.js
+++ src/radicchio.js
@@ -2,6 +2,7 @@ require('babel-core/register');
 import Redis from 'ioredis';
 import fs from 'fs';
 import Promise from 'bluebird';
+import _ from 'lodash';
 
 const redis = new Redis();
 const radicchio = {};
@@ -14,6 +15,7 @@ radicchio.init = function () {
   return new Promise(function (resolve) {
     const startFile = loadLuaFile(__dirname + '/lua/start.lua');
     const disableFile = loadLuaFile(__dirname + '/lua/disable.lua');
+    const getTimeLeftFile = loadLuaFile(__dirname + '/lua/getTimeLeft.lua');
 
     redis.defineCommand('startTimer', {
       numberOfKeys: 1,
@@ -23,6 +25,11 @@ radicchio.init = function () {
     redis.defineCommand('disableTimer', {
       numberOfKeys: 1,
       lua: disableFile,
+    });
+
+    redis.defineCommand('getTimeLeft', {
+      numberOfKeys: 1,
+      lua: getTimeLeftFile,
     });
 
     resolve(true);
@@ -57,6 +64,47 @@ radicchio.disableTimer = function (id) {
         else {
           resolve(result);
         }
+      });
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
+
+radicchio.getTimeLeft = function (id) {
+  return new Promise(function (resolve, reject) {
+    try {
+      redis.getTimeLeft(id, '', function (err, result) {
+        if (err) {
+          reject(err);
+        }
+        else if (result >= 0) {
+          resolve(result);
+        }
+        else if (result < 0) {
+          reject('The timer with key ' + id + 'has expired or does not exist');
+        }
+      });
+    }
+    catch (e) {
+      reject(e);
+    }
+  });
+};
+
+radicchio.getTimeLeftOnAllKeys = function (ids) {
+  const promises = [];
+
+  return new Promise(function (resolve, reject) {
+    try {
+      _.map(ids, function (id) {
+        promises.push(radicchio.getTimeLeft(id));
+      });
+
+      Promise.all(promises)
+      .then(function (results) {
+        resolve(results);
       });
     }
     catch (e) {
