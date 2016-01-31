bc0b607f21b6613786de95f1450518e72fa43486
Eric Kipnis <esk8107@rit.edu>
2016-01-31 14:43:39 -0500

    Fix index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('babel-core/register');
-const radicchio = require('./dist/app.js');
 
-radicchio.init();
+module.exports = function(redisUrl) {
+  const redisURL = redisUrl || 'redis://localhost:6379'
+  const radicchio = require('./dist/app.js')(redisURL);
+  radicchio.init();
 
-module.exports = radicchio;
+  return radicchio;
+}
